4df3382e2eb09f349548caaef9322d329cebeaf1
Radhika <radhikakhatiwoda666@gmail.com>
2024-10-24 21:47:25 +0545

    ep3

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
--- ecompkmc/settings.py
+++ ecompkmc/settings.py
@@ -117,11 +117,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticsc')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = os.path.join()
+STATICFILES_DIRS = ['static/']
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
