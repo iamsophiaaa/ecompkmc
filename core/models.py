ce081ae980815c1fcbd3179cad0be1ff43ebd4a2
Sophia Rai <sofia16388@gmail.com>
2024-10-24 22:19:13 +0545

    commit

--- core/models.py
+++ core/models.py
@@ -1,7 +1,9 @@
 import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
@@ -18,13 +20,29 @@ class Customer(models.Model):
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
     
+class Seller(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=50)
+    last_name  = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+    
 class Product(models.Model):
+    CONDITION_CHOICES = [
+        ('new', 'New'),
+        ('second_hand', 'Second_Hand'),
+       
+    ]
     name = models.CharField(max_length=50)
     price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default= 1)
     image = models.ImageField(upload_to='uploads/product')
     description =models.CharField(max_length=200, default='', blank=True, null=True)
-
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE , default=1) 
     def __str__(self):
         return self.name
     
@@ -38,4 +56,5 @@ class Order(models.Model):
     status = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.product
+       return f'Order for {self.product.name} by {self.customer.first_name}'
+    
--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
