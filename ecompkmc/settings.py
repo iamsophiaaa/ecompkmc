85c68e6634f3f3f38e9d59fd6918884997b02061
Sophia Rai <sofia16388@gmail.com>
2024-10-24 12:55:12 +0545

    day2

--- ecompkmc/settings.py
+++ ecompkmc/settings.py
@@ -120,9 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticsc')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = os.path.join()
+MEDIA_URL = "media/"
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
