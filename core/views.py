56eb5997d558c50eff25f302909ccee9ff73168d
Sophia Rai <sofia16388@gmail.com>
2024-10-25 16:18:11 +0545

    commit

--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate , login, logout
 from django.contrib import messages
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
 # Create your views here.
 def home(request):
     products = Product.objects.all()
@@ -35,3 +37,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You've been logged out."))
     return redirect('home')
+
+def register_user(request):
+   
+    return render(request, 'register_user.html', {})
