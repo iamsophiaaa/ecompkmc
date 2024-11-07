9c4141d532bc5a3c55f5d585a3868ca2f34ffbc5
Radhika <radhikakhatiwoda666@gmail.com>
2024-11-07 18:34:46 +0545

    update cart

--- cart/cart.py
+++ cart/cart.py
@@ -8,12 +8,13 @@ class Cart():
             cart=self.session['session_key']={}
             self.cart=cart
 
-    def add(self,product):
+    def add(self,product,quantity):
         product_id=str(product.id)
+        product_qty=str(quantity)
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id]={'price':str(product.price)}
+            self.cart[product_id]=int(product_qty)
             self.session.modified=True
     def __len__(self):
         return len(self.cart)
@@ -21,5 +22,8 @@ class Cart():
     def get_products(self):
         product_id=self.cart.keys()
         products=Product.objects.filter(id__in=product_id)
-        return products           
+        return products    
+    def get_quants(self):
+        quantities=self.cart
+        return quantities       
     
